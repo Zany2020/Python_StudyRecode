527b1c1e6f86d47024c2795a95fc453ef0e9f8c7
雨田波 <zany.0710@gmail.com>
2025-12-23 22:48:14 +0800

    (⊙ˍ⊙)？

---
+++ 001.py
@@ -0,0 +1,33 @@
+# print("Hello World")
+# print("I Love Rem'rem'\n")
+# print('I Love Rem"rem"')
+# print('I Love Rem\'rem"')#
+# my_love = "rem"
+# print("masahiro" + my_love)
+#
+# import math
+# result = math.log2(8)
+# print(result)
+#
+# lalalai = 'Raftaria'[2]
+# lalalaii = "Raftaria"[2]
+# print(lalalai)
+# print(lalalaii)
+# print(type(my_love))
+#
+# jiji = input("请输入你喜欢的人的名字：")W
+# print(jiji)
+# print(input)#错误
+
+# rem_hight = float(input("请输入rem的身高："))
+# rem_weigth = float(input("请输入rem的体重："))
+# rem_bmi = rem_weigth / (rem_hight) ** 2
+# # print("rem的BMI为：" + rem_bmi)
+# print("rem的BMI为：" + str(rem_bmi))
+# #这里要转化成字符串类型才可以打印
+
+rem = 2020
+if (rem == 2020):
+    print("rem")
+if rem == 2020:
+    print("rem")
